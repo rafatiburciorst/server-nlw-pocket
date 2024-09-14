276dff7f478bdc06dfcb272e69a1f59e100462b5
Rafael Tiburcio <rafael.tiburcio@live.com>
2024-09-14 09:29:35 -0300

    finished backend server

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,21 @@
-import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
 import { fastify } from 'fastify'
+import fastifyCors from '@fastify/cors'
 import { createGoalRoute } from './routes/create-goals.js'
 import { createCompletionRoute } from './routes/create-completion.js'
 import { getPendingGoalRoute } from './routes/get-pending-goal.js'
 import { getWeekSummaryRoute } from './routes/get-week-summary.js'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
+
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
@@ -14,7 +24,8 @@ app.register(createCompletionRoute)
 app.register(getPendingGoalRoute)
 app.register(getWeekSummaryRoute)
 
-
-app.listen({
-    port: 3333
-}).then(() => console.log('http server is running'))
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log('http server is running'))
